c803ef091241359f7d6a04b309523fa94c42aa49
Arif-Zaman10 <arif.techspace@gmail.com>
2026-02-28 14:15:16 +0600

    Etch-A-Sketch

---
+++ Etch-A-Sketch/main.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+
+tim  = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(50)
+
+def move_backward():
+    tim.backward(50)
+
+def move_right():
+    new_heading = tim.heading() + 50
+    tim.setheading(new_heading)
+
+def move_left():
+    new_heading = tim.heading() - 50
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
